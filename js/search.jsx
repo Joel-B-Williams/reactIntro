721126ccfca9e30a1496624e2387070c1be939f4
Joel <joel.b.williams.is@gmail.com>
2017-10-02 15:43:20 -0500

    include img/info in Search

--- js/search.jsx
+++ js/search.jsx
@@ -6,8 +6,17 @@ const Search = () => (
 	<div className='search'>
 		{/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
 			cool trick to preview data */}
-		{preload.shows.map((show) => <h3>{show.title}</h3>)}
+		{preload.shows.map((show) => (
+			<div className='show-card'>
+				<img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
+				<div>
+					<h3>{show.title}</h3>
+					<h4>({show.year})</h4>
+					<p>{show.description}</p>
+				</div>
+			</div>
+		))}
 	</div>
-	)
+);
 
 export default Search;
