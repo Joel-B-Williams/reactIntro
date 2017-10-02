e4be4643191d6bc43d9810fd988a5f46654f3140
Joel <joel.b.williams.is@gmail.com>
2017-10-02 15:49:50 -0500

    pull out Showcard component

--- js/search.jsx
+++ js/search.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
+import Showcard from './showcard';
 import preload from '../data';
+
 
 const Search = () => (
 	<div className='search'>
 		{/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
 			cool trick to preview data */}
-		{preload.shows.map((show) => (
-			<div className='show-card'>
-				<img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
-				<div>
-					<h3>{show.title}</h3>
-					<h4>({show.year})</h4>
-					<p>{show.description}</p>
-				</div>
-			</div>
-		))}
+		{preload.shows.map((show) => (<Showcard show={show} />))}
 	</div>
 );
 
