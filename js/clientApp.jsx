cb98a6495aa35bd9e441f611152c37aa2fb10d89
Joel <joel.b.williams.is@gmail.com>
2017-10-01 21:14:49 -0500

    webpack server with /public relative path

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-const MyTitle = function(props) {
-  // props = variable like anything else, but convention says 'use props'
-  // type, attributes/props, children
-  return React.createElement('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+// no curlies because this is a single-line (technically) and uses implicit return
+const App = () => (
+  <div className='app' >
+    <div className='landing' >
+      <h1>svideo</h1>
+      <input type='text' placeholder='Search' />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyFirstComponent = function() {
-  return React.createElement(
-    'div',
-    null,
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'BlanchedAlmond' }),
-    ce(MyTitle, { title: 'Rick & Morty', color: 'RebeccaPurple' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'Peru' })
-  );
-};
+render(<App />, document.getElementById('app'));
 
-render(React.createElement(MyFirstComponent), document.getElementById('app'));
+
+// INTRO STUFFS & NOTES
+// const ce = React.createElement;
+
+// const MyTitle = function(props) {
+//   // props = variable like anything else, but convention says 'use props'
+//   // type, attributes/props, children
+//   // return React.createElement('div', null, ce('h1', { style: { color: props.color } }, props.title));
+//   // Well this looks familiar - .jsx allows you to write actual HTML instead of the above which is designed to mimick it.  Cool.
+//   return (
+//     <div>
+//       <h1 style={{color: props.color}}>{props.title}</h1>
+//     </div>
+//   );
+// };
+
+// const MyFirstComponent = function() {
+//   // return React.createElement(
+//   //   'div',
+//   //   null,
+//   //   ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+//   //   ce(MyTitle, { title: 'Silicon Valley', color: 'BlanchedAlmond' }),
+//   //   ce(MyTitle, { title: 'Rick & Morty', color: 'RebeccaPurple' }),
+//   //   ce(MyTitle, { title: 'Stranger Things', color: 'Peru' })
+//   // );
+//   return (
+//     <div id="my-first-component">
+//       <MyTitle title="Game of Thrones" color="YellowGreen"/>
+//       <MyTitle title="Silicon Valley" color="BlanchedAlmond"/>
+//       <MyTitle title="Rick & Morty" color="RebeccaPurple"/>
+//       <MyTitle title="Stranger Things" color="Peru"/>
+//       <MyTitle title="Ozark" color="Burlywood"/>
+//     </div>
+//   );
+// };
+
+// render(React.createElement(MyFirstComponent), document.getElementById('app'));
