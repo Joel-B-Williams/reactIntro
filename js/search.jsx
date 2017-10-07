4835c7832b2b4922242fb9fd55f618ef9d35a735
Joel <joel.b.williams.is@gmail.com>
2017-10-07 16:40:15 -0500

    add header"

--- js/search.jsx
+++ js/search.jsx
@@ -5,6 +5,10 @@ import preload from '../data';
 
 const Search = () => (
 	<div className='search'>
+		<header>
+			<h1>svideo</h1>
+			<input type='text' placeholder='Search' />
+		</header>
 		{/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
 			cool trick to preview data */}
 		<div>
