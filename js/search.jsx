d56cc4fa3295468a6606e2b6c69d0357e4fb1cd6
Joel <joel.b.williams.is@gmail.com>
2017-10-09 15:38:00 -0500

    create basic search filtering by search word(s)

--- js/search.jsx
+++ js/search.jsx
@@ -7,7 +7,7 @@ class Search extends Component {
 		super(props);
 
 		this.state = {
-			searchTerm: 'this is some sort of debug statement'
+			searchTerm: ''
 		};
 		// these bind the function to this component - can be done where function is called in render, technically, but then it's called any time you render (aka a lot) and that is horribly inefficient & resourse wasteful
 		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
@@ -28,7 +28,8 @@ class Search extends Component {
 
 		}
 		NOTE -> no bind called in class definition
-		Works because arrow functions do not create a new context, so there is no new 'this', and no need to bind to context of the class
+	  - Does not need to accept props because this is actually using default React Constructor, which takes them by default
+		- Works because arrow functions do not create a new context, so there is no new 'this', and no need to bind to context of the class
 	*/
 
 	handleSearchTermChange(event) {
@@ -50,7 +51,9 @@ class Search extends Component {
 				{/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
 	cool trick to preview data */}
 				<div>
-					{preload.shows.map((show) => (<Showcard key={show.imdbID} {...show} />))}
+					{preload.shows
+					.filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+					.map((show) => (<Showcard key={show.imdbID} {...show} />))}
 				</div>
 			</div>
 		);
