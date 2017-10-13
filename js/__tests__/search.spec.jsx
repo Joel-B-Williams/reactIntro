06f21349cdf2a6025b0949902fb512c40893fc43
Joel <joel.b.williams.is@gmail.com>
2017-10-13 15:08:16 -0500

    add modules/etc for first passing snapshot test

--- js/__tests__/search.spec.jsx
+++ js/__tests__/search.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderer from 'react-tests-renderer';
-import Search from '../Search';
+import renderer from 'react-test-renderer';
+import Search from '../search';
 
 //this is (currently) just jasmine, so you can do describe('') do//end if you want 
 test('Search renders correctly', () => {
