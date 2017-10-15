3b0aea9524ebd43905b50f9633eebd3dc796dddb
Joel <joel.b.williams.is@gmail.com>
2017-10-14 19:07:48 -0500

    fix formatting

--- js/app.jsx
+++ js/app.jsx
@@ -6,16 +6,17 @@ import Search from './search';
 // no curlies because this is a single-line (technically) and uses implicit return
 const FourOhFour = () => <h1>404</h1>;
 
-const App = () =>
-	<BrowserRouter>
-		<div className="app">
-			<Switch>
-				{/* this is how you write comments inside here*/}
-				<Route exact path="/" component={Landing} />
-				<Route path="/search" component={Search} />
-				<Route component={FourOhFour} />
-			</Switch>
-		</div>
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        {/* this is how you write comments inside here*/}
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
   </BrowserRouter>;
-  
-  export default App;
+);
+
+export default App;
