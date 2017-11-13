4cdb87c2e2441f7e67cca510d5402d45a19780d1
Joel <joel.b.williams.is@gmail.com>
2017-11-13 13:51:50 -0600

    route to details of given show by IMDB-ID

--- js/app.jsx
+++ js/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './landing';
 import Search from './search';
 import Details from './details';
+import preload from '../data.json';
 
 // no curlies because this is a single-line (technically) and uses implicit return
 const FourOhFour = () => <h1>404</h1>;
@@ -14,7 +15,12 @@ const App = () => (
         {/* this is how you write comments inside here*/}
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route path="/details/:id" component={Details} />
+      {/*pass Details as a component so you can give it props, in this case a function to find the correct show by imbdID*/}
+        <Route 
+          path="/details/:id" 
+          component={props => 
+            <Details 
+              show={preload.shows.find((show) => props.match.params.id === show.imdbID)} {...props} />} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
