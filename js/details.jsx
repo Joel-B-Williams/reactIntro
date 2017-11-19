2e6cc34edb10eaf7eecd3c08c32f7051fffe74fb
Joel <joel.b.williams.is@gmail.com>
2017-11-19 17:22:06 -0600

    change details to class-type component

--- js/details.jsx
+++ js/details.jsx
@@ -1,30 +1,35 @@
 // @flow
 
-import React from 'react'
+import React, { Component } from 'react';
 import Header from './header';
 
-const Details = (props: { show: Show }) => {
+class Details extends Component {
+	props: {
+		show: Show
+	}
+	render(){
 	// quick way to set each of these values inside props.show to their respective names
-	const { title, description, year, poster, trailer } = props.show;
-	return (
-		<div className='details'>
-			<Header />
-			<section>
-				<h1>{title}</h1>
-				<h2>({year})</h2>
-				<img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`}/>
-				<p>{description}</p>
-			</section>
-			<div>
-				<iframe
-					src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-					frameBorder='0'
-					allowFullScreen
-					title={`Trailer for ${title}`}
-				/>
+		const { title, description, year, poster, trailer } = this.props.show;
+		return (
+			<div className='details'>
+				<Header />
+				<section>
+					<h1>{title}</h1>
+					<h2>({year})</h2>
+					<img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`}/>
+					<p>{description}</p>
+				</section>
+				<div>
+					<iframe
+						src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+						frameBorder='0'
+						allowFullScreen
+						title={`Trailer for ${title}`}
+					/>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	};
+}
 
 export default Details;
