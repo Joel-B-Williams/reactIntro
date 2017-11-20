91c9b432d2f8561d52805b69ee230f568ac57ab5
Joel <joel.b.williams.is@gmail.com>
2017-11-20 16:09:50 -0600

    landing now listens to Redux for searchTerm - but nothing updates redux

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
-const Landing = () =>
+const Landing = (props: {searchTerm: string}) =>
 	<div className="landing">
 		<Header />
-		<input type="text" placeholder="Search" />
+		<input value={props.searchTerm} type="text" placeholder="Search" />
 		<Link to="/search">or Browse All</Link>
 	</div>;
 
-export default Landing;
+// putting parens around the object tells react you're actually returning an object literal instead of declaring a function body
+// this takes the full Redux state & pulls out only the part landing cares about (searchTerm).  Connected with connect below
+const mapStateToProps = (state) => ({ searchTerm: state.searchTerm });
+
+//connect(<thing>) calls a function that runs on result of the param (Landing in this case)
+export default connect(mapStateToProps)(Landing);
