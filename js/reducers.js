bfb729309628c975e3e902a8efd4eaea015275ae
Joel <joel.b.williams.is@gmail.com>
2017-11-20 15:35:58 -0600

    action notes & create store.js

--- js/reducers.js
+++ js/reducers.js
@@ -2,6 +2,12 @@ const DEFAULT_STATE = {
 	searchTerm: ''
 };
 
+// Actions are essentially an object - this syntax is 'flux standard payload' & is a good standard to use (example below)
+// {
+// 	type: string,
+// 	payload: 
+// }
+
 // root store - takes in action & state, returns state.  ZERO side effects.
 // Root reducer must take care of default state (it is initially called with no state, so needs a default)
 // Root Reducer must be able to handle unknown actions (action.type bit)
---
+++ js/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+// basic store - preeeetty simple.  Middleware & such would go here as well.
+
+const store = createStore(reducer)
+
+export default store;
