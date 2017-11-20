f506f3ad415b6468b9683f97d86837cf44d787da
Joel <joel.b.williams.is@gmail.com>
2017-11-20 16:23:27 -0600

    full redux loop on landing page search box

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import { setSearchTerm } from './actionCreators';
 
-const Landing = (props: {searchTerm: string}) =>
+const Landing = (props: {searchTerm: string, handleSearchTermChange: Function}) =>
 	<div className="landing">
-		<Header />
-		<input value={props.searchTerm} type="text" placeholder="Search" />
+		<h1>{props.searchTerm || 'svideo'}</h1>
+		<input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
 		<Link to="/search">or Browse All</Link>
 	</div>;
 
@@ -14,5 +15,14 @@ const Landing = (props: {searchTerm: string}) =>
 // this takes the full Redux state & pulls out only the part landing cares about (searchTerm).  Connected with connect below
 const mapStateToProps = (state) => ({ searchTerm: state.searchTerm });
 
-//connect(<thing>) calls a function that runs on result of the param (Landing in this case)
-export default connect(mapStateToProps)(Landing);
+// creates functions to dispatch events to Redux (so it can update its store)
+// returns an object that will be injected as props into Landing
+// passes event.target.value into setSearchTerm, which formats that into an action (see actionCreators.js) of type SET_SEARCH_TERM
+const mapDispatchToProps = (dispatch: Function) => ({
+	handleSearchTermChange(event) {
+		dispatch(setSearchTerm(event.target.value));
+	}
+});
+
+//connect(<thing>) calls a function that runs on result of the param (Landing in this case) - injects results so they can be used as props
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
